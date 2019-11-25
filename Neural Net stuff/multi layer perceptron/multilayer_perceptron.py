1468e1aed93d2095f716507941cd62826427c06e
dfloydCSUB <dfloyd1@csub.edu>
2019-11-24 17:34:03 -0800

    fully working (i think?) MAD drops ~0.1 per 100 epochs

--- Neural Net stuff/multi layer perceptron/multilayer_perceptron.py
+++ Neural Net stuff/multi layer perceptron/multilayer_perceptron.py
@@ -22,12 +22,12 @@ with open(file, newline='') as csvfile:
 seed(1) # seed rand # generator
 #w = [random() for i in range(4)]
 #theta = 0
-alpha = 0.08
+alpha = 0.1
 y = []
 yd = []
 
 class neuron:
-    def __init__(self, dSize, inputData):
+    def __init__(self, dSize):
         self.dSize = dSize
         self.weights = []
         #self.weights = []
@@ -59,65 +59,72 @@ class neuron:
     def updateOWeights(self, D):
         delta = D
         for w in range(len(self.weights)):
-            weight = self.weights[w]
-            weight = weight + alpha * (y[w-3]) * delta
+            oWeight = self.weights[w]
+            oWeight = oWeight + alpha * (y[w-3]) * delta
         self.theta = self.theta + alpha * -1 * delta
 
-    def updateHWeights(self, D):
-        for w in rage(len(self.weights)):
-            
+    def updateHWeights(self, D, row):
+        delta = D
+        sRow = row
+        for w in range(len(self.weights)):
+            hWeight = self.weights[w]
+            hWeight = hWeight + alpha *(float(sRow[w]))
+        self.theta = self.theta + alpha * -1 * delta
 
 
 
 
 class layer: 
 
-    def __init__(self, lSize, sSize, inputData):
+    def __init__(self, lSize, sSize):
         self.lSize = lSize
         self.sSize = sSize
-        self.inputData = inputData
         self.n = []
         self.oDelta = []
         self.hDelta = []
-        self.prevWeights = []
 
-    def appendYd(self):
-        actual = self.inputData[self.sSize:]
+    def appendYd(self, sample):
+        sampleRow = sample
+        actual = sample[self.sSize:]
         for i in range(len(actual)):
             yd.append(float(actual[i]))
-
+        
     def createNeurons(self):
         for x in range(self.lSize):
-            self.n.append(neuron(self.sSize, self.inputData))
+            self.n.append(neuron(self.sSize))
         for x in range(self.lSize):
             self.n[x].initWeights()
 
-    def forward(self):
+    def forward(self, sample):
+        sampleRow = sample
         for j in range(self.lSize):
-            y.append(self.n[j].feedForward(self.inputData))
+            y.append(self.n[j].feedForward(sampleRow))
 
+    #start backpropogation
     def calcOdeltas(self):
         y.reverse()
-        self.n.reverse()
+        #self.n.reverse()
         yd.reverse()
-        yOut = self.n[:self.lSize]
+        yOut = len(self.n)
         
-        for i in range(len(yOut)):
+        for i in range(yOut):
             newODelta = (y[i] * (1-y[i]) * (yd[i]-y[i]))
             self.oDelta.append(newODelta)
         
         for d in range(len(self.oDelta)):
             self.n[d].updateOWeights(self.oDelta[d])
-    
-    def calcHdeltas(self):
-        yHidden = self.n[self.lSize:]
         
-        for i in range(len(yHidden)):
-            newHDelta = (y[i] * (1 - y[i]) * ((self.oDelta[i]*self.n[1].weights[i]) + (self.oDelta[i+1]*self.n[2].weights[i]) + (self.oDelta[i+2]*self.n[3].weights[i])))
+    
+    def calcHdeltas(self, sample):
+        yHidden = len(self.n)
+        sRow = sample
+        for i in range(yHidden):
+            newHDelta = (y[i] * (1 - y[i]) * ((output.oDelta[0]*output.n[0].weights[i]) + (output.oDelta[1]*output.n[1].weights[i]) + (output.oDelta[2]*output.n[2].weights[i])))
             self.hDelta.append(newHDelta)
 
         for d in range(len(self.hDelta)):
-            self.n[d].updateHWeights(self.hDelta[d])
+            self.n[d].updateHWeights(self.hDelta[d], sRow)
+        
 
 
 # TO DO:
@@ -126,34 +133,52 @@ class layer:
 #
 #       pass each layer's neurons and thier weights into something global
 #       in order to be able to pass values between layers
-#       currently, each layer's neurons are unable to pass data back and forth.
+#       currently, each layer's neurons are unable to pass data back and forth. -- ALTERNATIVE SOL. FOUND
+#
+#       figure out why MAD won't get smaller
 
 
 
 #main body
 
-for epoch in range(1):
+#hidden layer definition
+hidden = layer(3, 4)    
+hidden.createNeurons()
+#output layer definition
+output = layer(3, 3)
+output.createNeurons()
+
+for epoch in range(100):
+    e = 0
+    count = 0
+    
     
     for sample in data:    
 
-        #hidden layer definition
-        hidden = layer(3, 4, sample)    
-        hidden.createNeurons()
-        
-        hidden.forward()
-        hidden.appendYd()
+        hidden.forward(sample)
+        hidden.appendYd(sample)
 
-        #output layer definition
-        output = layer(3, 3, y)
-        output.createNeurons()
-        output.forward()
+        output.forward(y)
 
+        #calc deltas and update weights
         output.calcOdeltas()
-        hidden.calcHdeltas()
+        hidden.calcHdeltas(sample)
+        
+        
+        output.oDelta.clear()
+        hidden.hDelta.clear()
+        
+        #error sum for mad calc
+        e += (abs(yd[0]-y[0]) + abs(yd[1]-y[1]) + abs(yd[2]-y[2])) 
+        count += 1
 
         #clear out y and yd after operating on each sample row in the data
-        #print(hidden.delta)
-        #print(". . . . . . . . . . .")
-        #print(output.delta)
-    #print(hidden.n[1].weights)
+        
+        yd.clear()
+        y.clear()
+    
+    #calc MAD
+    mad = (1/count) * e
+
+    print("MAD: " + str(mad))
     
